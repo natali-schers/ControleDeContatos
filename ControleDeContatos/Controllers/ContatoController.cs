7d70374f5037af5df2dd0f91c98977eeff86cb61
natali-schers <schersnatali@gmail.com>
2023-05-25 22:04:15 -0300

    Add project files.

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Criar()
+        {
+            return View();
+        }
+        
+        public ActionResult Editar()
+        {
+            return View();
+        }
+        
+        public ActionResult Apagar()
+        {
+            return View();
+        }
+
+        public ActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
